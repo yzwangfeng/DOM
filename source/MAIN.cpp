ea8312d334e3dec9cdf009d6c79382ac21e66f67
PKU-ZLR <55825746+PKU-ZLR@users.noreply.github.com>
2019-10-08 15:54:08 +0800

    Add files via upload

--- source/MAIN.cpp
+++ source/MAIN.cpp
@@ -28,6 +28,17 @@ void get_file_name(string path, vector<string> &files) {
     closedir(dir);
 }
 
+bool cmp_Depth(Cut A, Cut B)
+{
+    return A.mindep < B.mindep || (A.mindep == B.mindep && A.AreaFlow < B.AreaFlow) || (A.mindep == B.mindep && A.AreaFlow == B.AreaFlow && A.Area < B.Area);
+}
+
+bool cmp_Depth2(Cut A, Cut B)
+{
+    return A.mindep < B.mindep || (A.mindep == B.mindep && A.Area < B.Area) || (A.mindep == B.mindep && A.Area == B.Area && A.AreaFlow < B.AreaFlow);
+}
+
+
 void Output(Circuit c) {
      queue <string> Q;
      int area = 0;
@@ -51,7 +62,8 @@ int main(int argc, char *argv[]) {
     queue <string> Q;
     map<string, int> ind;
     int Luts = 4;
-    string dir = "benchmark/IWLS93";
+    unsigned int C = 8; // record the most C Cuts
+    string dir = "benchmark/test1";
     get_file_name(dir, benchmark);
 
     for (string str : benchmark) {
@@ -108,8 +120,9 @@ int main(int argc, char *argv[]) {
             }
 
 	    /*Calculate dep, areaflow, exact area*/	
+	    vector<Cut> Cuts;
 	    c.graph[now]->mindep = INF;
-	    c.graph[now]->Area = INF;
+            c.graph[now]->Area = INF;
             c.graph[now]->AreaFlow = inf;
 	    for (set<string> ct : c.graph[now]->cuts) {
 		int dep = 0, area = 0;
@@ -122,35 +135,53 @@ int main(int argc, char *argv[]) {
 		dep++, area++;
 		AF /= max(1.0, 1.0 * c.graph[now]->suc.size());
 
+		Cuts.push_back(Cut(ct, dep, area, AF));		
+		
 		/*Compare dep*/
-		if (dep < c.graph[now]->mindep) {
+		/*if (dep < c.graph[now]->mindep) {
 		    c.graph[now]->mindep = dep;
 		    c.graph[now]->AreaFlow = AF;
 		    c.graph[now]->Area = area;
 		    c.graph[now]->Rcut = ct;
 		    continue;
-                }
+                }*/
 
 		/*Compare Area Flow*/
-		if (dep == c.graph[now]->mindep && AF < c.graph[now]->AreaFlow) {
+		/*if (dep == c.graph[now]->mindep && AF < c.graph[now]->AreaFlow) {
 		    c.graph[now]->mindep = dep;
                     c.graph[now]->AreaFlow = AF;
                     c.graph[now]->Area = area;
                     c.graph[now]->Rcut = ct;
                     continue;
-                }
+                }*/
 
 		/*Compare Area*/
-                if (dep == c.graph[now]->mindep && AF == c.graph[now]->AreaFlow && area < c.graph[now]->Area) {
+                /*if (dep == c.graph[now]->mindep &
+ * & AF == c.graph[now]->AreaFlow && area < c.graph[now]->Area) {
                     c.graph[now]->mindep = dep;
                     c.graph[now]->AreaFlow = AF;
                     c.graph[now]->Area = area;
                     c.graph[now]->Rcut = ct;
                     continue;
-                }
+                }*/
 
 	    }
-
+	    sort(Cuts.begin(), Cuts.end(), cmp_Depth);
+	    c.graph[now]->cuts.clear();
+	    unsigned int len = Cuts.size();
+            //cout << "now:" << now << endl;
+	    for (unsigned int i = 0; i < min(C, len); i++)
+            {
+		c.graph[now]->cuts.insert(Cuts[i].names);
+		//cout << Cuts[i].mindep << endl;
+            }
+	    c.graph[now]->cuts.insert(set<string>{now});
+            /*for (string s : Cuts[0].names)
+		cout << s << endl;*/
+	    c.graph[now]->mindep = Cuts[0].mindep;
+            c.graph[now]->AreaFlow = Cuts[0].AreaFlow;
+            c.graph[now]->Area = Cuts[0].Area;
+            c.graph[now]->Rcut = Cuts[0].names;
         }
 	Output(c);
         clock_t finish = clock();
