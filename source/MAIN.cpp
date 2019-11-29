77479cc6d04e71e46a7802dc089bb65a2da55aed
PKU-ZLR <55825746+PKU-ZLR@users.noreply.github.com>
2019-11-30 01:57:50 +0800

    2 outputs upload

--- source/MAIN.cpp
+++ source/MAIN.cpp
@@ -6,6 +6,7 @@
 #include "ABC.h"
 #include "Circuit.h"
 #include <map>
+#include <set>
 #include <dirent.h>
 #include <queue>
 #include <cstdio>
@@ -20,11 +21,10 @@ using namespace std;
 
 FILE *out1, *out2;
 int Luts = 6, Dep = 0;
-map<string, bool> vis;
-vector<Cut> L[MAXDEP];
+map<string, int> vis;
 
 void get_file_name(string path, vector<string> &files) {
-    struct dirent *ptr;
+	struct dirent *ptr;
     DIR *dir = opendir(path.c_str());
     while ((ptr = readdir(dir)) != NULL) {
         if (ptr->d_name[0] != '.') {  // skip . and ..
@@ -36,27 +36,26 @@ void get_file_name(string path, vector<string> &files) {
 
 bool cmp_Depth(Cut A, Cut B) {
     return A.mindep < B.mindep || (A.mindep == B.mindep && A.AreaFlow < B.AreaFlow)
-            || (A.mindep == B.mindep && A.AreaFlow == B.AreaFlow && A.Area < B.Area);
+		|| (A.mindep == B.mindep && A.AreaFlow == B.AreaFlow && A.Area < B.Area);
 }
 
 bool cmp_Depth2(Cut A, Cut B) {
     return A.mindep < B.mindep || (A.mindep == B.mindep && A.Area < B.Area)
-            || (A.mindep == B.mindep && A.Area == B.Area && A.AreaFlow < B.AreaFlow);
+		|| (A.mindep == B.mindep && A.Area == B.Area && A.AreaFlow < B.AreaFlow);
 }
+
 
 bool Dcmp_Depth(DoubleCut A, DoubleCut B) {
     return A.mindep < B.mindep || (A.mindep == B.mindep && A.AreaFlow < B.AreaFlow)
-            || (A.mindep == B.mindep && A.AreaFlow == B.AreaFlow && A.Area < B.Area);
+		|| (A.mindep == B.mindep && A.AreaFlow == B.AreaFlow && A.Area < B.Area);
 }
 
 bool Dcmp_Depth2(DoubleCut A, DoubleCut B) {
     return A.mindep < B.mindep || (A.mindep == B.mindep && A.Area < B.Area)
-            || (A.mindep == B.mindep && A.Area == B.Area && A.AreaFlow < B.AreaFlow);
+		|| (A.mindep == B.mindep && A.Area == B.Area && A.AreaFlow < B.AreaFlow);
 }
 
 void Output(Circuit& c) {
-    map<string, bool> Flag;
-    Flag.clear();
     queue<string> Q;
     vis.clear();
     int area = 0, dep = 0;
@@ -85,47 +84,106 @@ void Output(Circuit& c) {
     c.Dep = dep;
 }
 void Output2(Circuit& c) {
-    map<string, bool> Flag;
-    Flag.clear();
-    queue<string> Q;
+    multiset<Cut> Q;
     vis.clear();
     int area = 0;
     for (string s : c.output) {
-        Q.push(s);
-        vis[s] = 1;
+		if (c.graph[s]->pre.size() == 0) continue;
+        Q.insert(c.graph[s]->Fcut);
+		vis[s] = 1;
+		//cout << c.graph[s]->Fcut.Name << endl;
+		//cout << c.graph[s]->Fcut.mindep << endl;
     }
+    //printf("%d\n", Q.size());
     while (!Q.empty()) {
-        string now = Q.front();
-        Q.pop();
+		//printf("start\n");
+        set<Cut>::iterator nw = Q.begin();
+		string now = nw->Name;
+        Q.erase(nw);
+		//cout << now << endl;
         if (c.graph[now]->pre.size() == 0)
             continue;
-	if (vis[now] > 1) continue;
-        fprintf(out2, "input:\n");
-        area++;
-	if (c.graph[now]->Rdcut.empty()) {
-            for (string st : c.graph[now]->Rcut) {
-                fprintf(out2, "%s ", st.c_str());
-                if (!vis[st]) {
-                    Q.push(st);
-                    vis[st] = 1;
-                }
-            }
-            fprintf(out2, "%s\n", ("\noutput:\n" + now).c_str());
-        }else {
-	    printf("Hello\n");
-            for (string st : c.graph[now]->Rdcut) {
-                fprintf(out2, "%s ", st.c_str());
-                if (!vis[st]) {
-                    Q.push(st);
-                    vis[st] = 1;
-                }
-            }
-	    string p = c.graph[now]->Partner; 
-	    vis[p] = 2;
-	    printf("%s %s\n", now.c_str(), p.c_str());
-            fprintf(out2, "%s\n", ("\noutput:\n" + now + " " + p).c_str());
-        }
-    }
+		if (c.graph[now]->Rcut.size() == Luts) {
+			fprintf(out2, "input:\n");
+			area++;
+			for (string st : c.graph[now]->Rcut) {
+				fprintf(out2, "%s ", st.c_str());
+				if (!vis[st]) {
+					Q.insert(c.graph[st]->Fcut);
+					vis[st] = 1;
+				}
+			}
+			fprintf(out2, "%s\n", ("\noutput:\n" + now).c_str());
+		} else {
+			vector<DoubleCut> L;
+			L.clear();
+			for (Cut itr : Q) {
+				//cout << "HH" << endl;
+				string nxt = itr.Name;
+				if (nxt == now) continue;
+				//cout << "HH" + nxt << endl;
+				set<string> tp;
+				tp.clear();
+				tp.insert(c.graph[now]->suc.begin(), c.graph[now]->suc.end());
+				tp.insert(c.graph[nxt]->suc.begin(), c.graph[nxt]->suc.end());
+				int sucsize = tp.size();
+				//printf("%d %s\n", sucsize, nxt.c_str());
+				for (int i = 2; i < Luts; i++) {
+					for (set<string> ct : c.graph[nxt]->cuts[i]) {
+						set<string> tmp = c.graph[now]->Rcut;
+						tmp.insert(ct.begin(), ct.end());
+						if (tmp.size() >= Luts) continue;
+						int dep = 0, area = 0;
+						double AF = 0;
+						for (string nt : tmp) {
+							dep = max(dep, c.graph[nt]->mindep);
+							area += c.graph[nt]->Area;
+							AF += c.graph[nt]->AreaFlow;
+						}
+						dep++, area++;
+						AF /= max(1.0, 1.0 * sucsize);
+
+						L.push_back(DoubleCut(tmp, now, nxt, dep, area, AF));
+					}
+				}
+			}
+			//printf("size %d\n", L.size());
+			sort(L.begin(), L.end(), Dcmp_Depth2);
+			if (L.size() == 0) {
+				fprintf(out2, "input:\n");
+				area++;
+				for (string st : c.graph[now]->Rcut) {
+					fprintf(out2, "%s ", st.c_str());
+					if (!vis[st]) {
+						Q.insert(c.graph[st]->Fcut);
+						vis[st] = 1;
+					}
+				}
+				fprintf(out2, "%s\n", ("\noutput:\n" + now).c_str());
+			}
+			else {
+				fprintf(out2, "input:\n");
+				area++;
+				for (string st : L[0].names) {
+					//cout << st << endl;
+					fprintf(out2, "%s ", st.c_str());
+					if (!vis[st]) {
+						Q.insert(c.graph[st]->Fcut);
+						vis[st] = 1;
+					}
+				}
+				fprintf(out2, "%s\n", ("\noutput:\n" + L[0].Name1 + " " + L[0].Name2).c_str());
+				set<Cut>::iterator it;
+				//cout << "HH\n";
+				for (it = Q.begin(); it != Q.end(); it++)
+					if (it->Name == L[0].Name2) break;
+				Q.erase(it);
+			}
+		}
+		//Q.erase(nw);
+		//for (Cut stt : Q)
+		//	cout << stt.Name << endl;
+	}
     fprintf(stdout, "Area: %d\nDepth: %d\n", area, c.Dep);
 }
 int main(int argc, char *argv[]) {
@@ -137,9 +195,9 @@ int main(int argc, char *argv[]) {
     string outdir = "result/ISCAS85";
     get_file_name(dir, benchmark);
 
-//    for (string str : benchmark) {
-//	cout << str  << endl;
-	string str = "c1355.blif"; {
+    for (string str : benchmark) {
+		cout << str  << endl;
+		//string str = "cir1.v"; {
         double total_time;
         clock_t start = clock();
 
@@ -149,21 +207,22 @@ int main(int argc, char *argv[]) {
         out1 = fopen((outdir + "/" + str + ".out1").c_str(), "w");
         out2 = fopen((outdir + "/" + str + ".out2").c_str(), "w");
 
-	for (int i = 0; i < MAXDEP; i++)
-	    L[i].clear();
+		//for (int i = 0; i < MAXDEP; i++)
+		//	L[i].clear();
 
         for (pair<string, Var*> p : c.graph) {
             ind[p.first] = p.second->pre.size();
-	    p.second->mindep = 0;
-	    p.second->Rcut.clear();
-	    p.second->Rdcut.clear();
-	}
+			p.second->mindep = 0;
+			p.second->Rcut.clear();
+			//			p.second->Rdcut.clear();
+		}
 
         for (string s : c.input) {           // Initial queue
             c.graph[s]->cuts[1].insert(set<string> { s });
             c.graph[s]->mindep = 0;
             c.graph[s]->Area = 0;
             c.graph[s]->AreaFlow = 0;
+			c.graph[s]->Fcut = Cut(set<string>{s}, s, 0, 0, 0);
             for (string nt : c.graph[s]->suc) {
                 ind[nt]--;
                 if (ind[nt] == 0)
@@ -181,22 +240,22 @@ int main(int argc, char *argv[]) {
             if (c.graph[now]->pre.size() == 2) {
                 string input1 = c.graph[now]->pre[0];
                 string input2 = c.graph[now]->pre[1];
-		for (int t = 1; t <= Luts; t++)
-	       	    for (set<string> c1 : c.graph[input1]->cuts[t]) {
-			for (int k = 1; k <= Luts; k++)
-                             for (set<string> c2 : c.graph[input2]->cuts[k]) {
+				for (int t = 1; t <= Luts; t++)
+					for (set<string> c1 : c.graph[input1]->cuts[t]) {
+						for (int k = 1; k <= Luts; k++)
+							for (set<string> c2 : c.graph[input2]->cuts[k]) {
                                 set<string> st = c1;
                                 st.insert(c2.begin(), c2.end());
                                 if (st.size() > (unsigned) Luts)
                                     continue;
                                 c.graph[now]->cuts[st.size()].insert(st);
                             }
-                        }
+					}
             }
 
             if (c.graph[now]->pre.size() == 1) {
                 string input1 = c.graph[now]->pre[0];
-		for (int k = 1; k <= Luts; k++)
+				for (int k = 1; k <= Luts; k++)
                     for (set<string> c1 : c.graph[input1]->cuts[k])
                         c.graph[now]->cuts[k].insert(c1);
             }
@@ -212,8 +271,8 @@ int main(int argc, char *argv[]) {
             c.graph[now]->mindep = INF;
             c.graph[now]->Area = INF;
             c.graph[now]->AreaFlow = inf;
-	    for (int k = 1; k <= Luts; k++) {
-	        for (set<string> ct : c.graph[now]->cuts[k]) {
+			for (int k = 1; k <= Luts; k++) {
+				for (set<string> ct : c.graph[now]->cuts[k]) {
                     int dep = 0, area = 0;
                     double AF = 0;
                     for (string nt : ct) {
@@ -225,91 +284,37 @@ int main(int argc, char *argv[]) {
                     AF /= max(1.0, 1.0 * c.graph[now]->suc.size());
 
                     Cuts[k].push_back(Cut(ct, now, dep, area, AF));
-		    Ct.push_back(Cut(ct, now, dep, area, AF));
-		}
+					Ct.push_back(Cut(ct, now, dep, area, AF));
+				}
             }
             for (int k = 1; k <= Luts; k++) {
                 sort(Cuts[k].begin(), Cuts[k].end(), cmp_Depth2);
             	c.graph[now]->cuts[k].clear();
             	unsigned int len = Cuts[k].size();
-            //cout << "now:" << now << endl;
+				//cout << "now:" << now << endl;
             	for (unsigned int i = 0; i < min(C, len); i++) {
                 	c.graph[now]->cuts[k].insert(Cuts[k][i].names);
-                //cout << Cuts[i].mindep << endl;
+					//cout << Cuts[i].mindep << endl;
             	}
             }		
             //if (c.graph[now]->pre.size() != 1)
             c.graph[now]->cuts[1].insert(set<string> { now });
-	    sort(Ct.begin(), Ct.end(), cmp_Depth2);
-	    int Depth = Ct[0].mindep;
+			sort(Ct.begin(), Ct.end(), cmp_Depth2);
+			int Depth = Ct[0].mindep;
             c.graph[now]->mindep = Ct[0].mindep;
             c.graph[now]->AreaFlow = Ct[0].AreaFlow;
             c.graph[now]->Area = Ct[0].Area;
             c.graph[now]->Rcut = Ct[0].names;
-            for (unsigned int i = 0; i < Ct.size(); i++) {
-		if (Ct[i].mindep == Depth)
-		   L[Depth].push_back(Ct[i]);
-		if (i > 40) break;
-            }
+			c.graph[now]->Fcut = Ct[0];
 		
             //cout << c.graph[now]->mindep << endl;
         }
         Output(c);
-	vector<DoubleCut> dc[MAXDEP];
-	for (int d = 1; d <= c.Dep; d++) {
-	    dc[d].clear();
-	    printf("%d\n", L[d].size());
-	    for (unsigned int i = 0; i < L[d].size(); i++) {
-		if (L[d][i].names.size() == (unsigned)Luts) continue;
-		for (unsigned int j = 0; j < L[d].size(); j++) {
-		    if (i == j) continue;
-		    if (L[d][i].Name == L[d][j].Name) continue;
-		    //printf("%d %d\n", i, j);
-		    set<string> st = L[d][i].names;
-		    st.insert(L[d][j].names.begin(), L[d][j].names.end());
-		    if (st.size() >= (unsigned)Luts) continue;
-		    string name1 = L[d][i].Name;
-		    string name2 = L[d][j].Name;
-                    int area = 0, dep = d;
-                    double AF = 0;
-                    for (string nt : st) {
-                        area += c.graph[nt]->Area;
-                        AF += c.graph[nt]->AreaFlow;
-                    }
-                    area++;
-		    /*Calculate Fout*/
-		    set<string> temp;
-		    temp.clear();
-		    for (string strg : c.graph[name1]->suc) temp.insert(strg);
-                    for (string strg : c.graph[name2]->suc) temp.insert(strg);
-		    /*              */
-		    AF /= max(1.0, 1.0 * temp.size());
-		    if (area < c.graph[name1]->Area + c.graph[name2]->Area /*|| 
-			(area == c.graph[name1]->Area + c.graph[name2]->Area && 
-			AF < c.graph[name1]->AreaFlow + c.graph[name2]->AreaFlow)*/)
-		    dc[d].push_back(DoubleCut(st, name1, name2, dep, area, AF)); 
-                }
-            }
-	    sort(dc[d].begin(), dc[d].end(), Dcmp_Depth2);
-	    printf("%d\n", dc[d].size());
-	    for (unsigned int i = 0; i < dc[d].size(); i++) {
-		string name1 = dc[d][i].Name1;
-		string name2 = dc[d][i].Name2;
-		if (!c.graph[name1]->Rdcut.empty() || !c.graph[name2]->Rdcut.empty()) continue;
-		//printf("%s %s\n", name1.c_str(), name2.c_str());
-		c.graph[name1]->Rdcut = dc[d][i].names;
-		c.graph[name2]->Rdcut = dc[d][i].names;
-		c.graph[name1]->Partner = name2;
-		c.graph[name2]->Partner = name1;
-            }
-        }
-	printf("Flag!!!\n");
         Output2(c);
-	//cout << c.abc_lut_area << endl;
         clock_t finish = clock();
         total_time = (double) (finish - start) / CLOCKS_PER_SEC;
         cout << "Run time: " << total_time << "s" << endl;
-   }
+	}
 
     return 0;
 }
